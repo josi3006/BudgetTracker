fed8bd3e3d06cf578a7948067cffbb36497aa048
JGSims <josi3006@hotmail.com>
2020-03-21 23:24:08 -0400

    Added MongoDB variables for Heroku

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+
 const PORT = 3000;
 
 const app = express();
@@ -15,10 +17,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(MONGODB_URI);
 
 // routes
 app.use(require("./routes/api.js"));
