7179a4ac4923efcab68c73a6155393b51f4781e7
JGSims <josi3006@hotmail.com>
2020-08-19 14:57:50 -0400

    Added function to clear form on submit

--- public/index.js
+++ public/index.js
@@ -114,6 +114,7 @@ function sendTransaction(isAdding) {
   transactions.unshift(transaction);
 
   // re-run logic to populate ui with new record
+  clearForm();
   populateChart();
   populateTable();
   populateTotal();
